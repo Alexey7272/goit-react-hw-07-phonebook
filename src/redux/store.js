c210585b3ec1f849af5b05cdd6cd28c5c9eecf9c
Alexey7272 <106250222+Alexey7272@users.noreply.github.com>
2023-04-04 14:52:11 +0300

    Delete persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
-import { configureStore, combineReducers  } from '@reduxjs/toolkit';
-import { contactReducer } from './contact/slice'
-import { filterReducer } from './filter/slice';
-import {contactApi} from './contacts/contactSlice';
+import {configureStore} from '@reduxjs/toolkit';
+// , combineReducers 
+// import { contactReducer } from './contact/slice'
+// import { filterReducer } from './filter/slice';
+// import {contactApi} from './contacts/contactSlice';
 
 export const store = configureStore({
     reducer: {},
